8f02cc6d8f420f219e514667692aa028f760064e
Prashant <prashantmmmut44@gmail.com>
2025-02-14 00:14:40 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import cx_freeze
+import sys
+import os 
+base = None
+
+
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+os.environ['TCL_LIBRARY'] = r"C:\Users\Admin\AppData\Local\Programs\Python\Python38\tcl\tcl8.6"
+os.environ['TK_LIBRARY'] = r"C:\Users\Admin\AppData\Local\Programs\Python\Python38\tcl\tk8.6"
+
+# executables = [cx_Freeze.Executable("Face_Recognition_Software.py", base=base, icon="face.ico")]
+
+
+cx_Freeze.setup(
+    name = "Smart Attendancre System Software",
+    options = {"build_exe": {"packages":["tkinter","os"], "include_files":["face.ico",'tcl86t.dll','tk86t.dll', 'college_images','data','database','attendance_report']}},
+    version = "1.0",
+    description = "Smart Attendace System ",
+    executables = executables
+    )
